48d45f24fb26ce16a9cd55fc6ed61fb27c27558b
kekland <kk.erzhan@gmail.com>
2022-03-02 14:31:33 +0100

    fix: analysis issues

--- test/views/checkout_with_new_card/cupertino_checkout_with_new_card_view_test.dart
+++ test/views/checkout_with_new_card/cupertino_checkout_with_new_card_view_test.dart
@@ -1,29 +1,9 @@
-import 'package:alchemist/alchemist.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ioka/ioka.dart';
-import 'package:ioka/src/api/generated/ioka_api.swagger.dart' as g;
 import 'package:provider/provider.dart';
 
 import '../../test_utils.dart';
 import '../view_test_utils.dart';
-
-Widget _buildWidget({
-  required String orderAccessToken,
-  required g.OrderOut order,
-  Brightness? brightness,
-}) {
-  return viewWrapper(
-    brightness: brightness,
-    child: ChangeNotifierProvider(
-      create: (_) => CheckoutWithNewCardModel(
-        orderAccessToken: orderAccessToken,
-        order: order,
-      ),
-      builder: (_, __) => const CupertinoCheckoutWithNewCardView(),
-    ),
-  );
-}
 
 Future<void> main() async {
   final orderAccessToken = await Ioka.api.asMock.mockCreateOrder(amount: 100);
--- test/views/save_card/cupertino_save_card_view_test.dart
+++ test/views/save_card/cupertino_save_card_view_test.dart
@@ -1,26 +1,8 @@
-import 'package:alchemist/alchemist.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ioka/ioka.dart';
 import 'package:provider/provider.dart';
 
-import '../../test_utils.dart';
 import '../view_test_utils.dart';
-
-Widget _buildWidget({
-  required String customerAccessToken,
-  Brightness? brightness,
-}) {
-  return viewWrapper(
-    brightness: brightness,
-    child: ChangeNotifierProvider(
-      create: (_) => SaveCardModel(
-        customerAccessToken: customerAccessToken,
-      ),
-      builder: (_, __) => const CupertinoSaveCardView(),
-    ),
-  );
-}
 
 Future<void> main() async {
   const customerAccessToken = 'token';
